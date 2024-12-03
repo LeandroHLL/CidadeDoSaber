f72751f9cf07c161b4afddd5d84079d2bd73edb0
LeoSenai <teste@senai.com>
2024-12-03 20:57:12 -0300

    update 2

--- app/views/auth/login.php
+++
@@ -1,8 +0,0 @@
-<?php include '../common/header.php'; ?>
-<h2>Login</h2>
-<form action="/login" method="post">
-    <input type="email" name="email" placeholder="E-mail" required>
-    <input type="password" name="senha" placeholder="Senha" required>
-    <button type="submit">Entrar</button>
-</form>
-<?php include '../common/footer.php'; ?>
