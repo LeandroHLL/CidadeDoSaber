06499070b5f6904e7fdd3e7dd1dc9ce3b1b2585d
LeandroHll <leandrohll@live.com>
2024-12-05 17:40:50 -0300

    titulo

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../../public/css/stylesign.css">
-  <title>Login</title>
+  <title>EducaNet | Login</title>
   <style>
     .error-message {
       color: white;
