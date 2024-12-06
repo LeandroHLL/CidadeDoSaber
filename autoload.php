38a4469a28efda5a67ccf4544598e4ebdcb81c5d
Luanxt4 <130299127+Luanxt4@users.noreply.github.com>
2024-12-06 00:00:34 -0300

    Add files via upload

---
+++ autoload.php
@@ -0,0 +1,39 @@
+<?php
+
+// Define the autoloader function
+function my_autoloader($class) {
+    
+    $baseNamespace = 'core\\model\\class';  // The namespace prefix
+ 
+    if (strpos($class, $baseNamespace) === 0) {
+        
+        $relativeClass = substr($class, strlen($baseNamespace));
+
+        $relativeClass = str_replace('\\', '/', $relativeClass);
+
+        $classFile = __DIR__ . '/core/model/class/' . $relativeClass . '.class.php';  // Assuming classes are inside the 'src' directory
+
+        if (file_exists($classFile)) {
+            require_once $classFile;
+        }
+    }
+    $baseNamespace = 'core\\controler\\class';
+    
+    if (strpos($class, $baseNamespace) === 0) {
+        
+        $relativeClass = substr($class, strlen($baseNamespace));
+        
+        $relativeClass = str_replace('\\', '/', $relativeClass);
+       
+        $classFile = __DIR__ . '/core/controler/class/' . $relativeClass . '.class.php'; 
+
+        if (file_exists($classFile)) {
+            require_once $classFile;  
+        }else{
+            echo"classe não encontada";
+        }
+    } 
+}
+
+
+spl_autoload_register('my_autoloader');
